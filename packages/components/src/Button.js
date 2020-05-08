c8645d2b385bb721eb1fadee0925bb17343d4daf
Dustin Senos <dustin@dustinsenos.com>
2020-05-08 12:04:24 -0700

    Added a shared component across the frontend and backend.

--- packages/components/src/Button.js
+++ packages/components/src/Button.js
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default ({ onClick, children }) => {
- return <button onClick={onClick}>{children}</button>
+ return (
+  <button style={{
+    padding: '10px',
+    color: 'white',
+    background: 'tomato',
+    borderRadius: '10px'
+  }} onClick={onClick}>
+    {children}
+  </button>
+ )
 }
